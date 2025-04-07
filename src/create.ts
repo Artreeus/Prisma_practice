20853e33d4a552ad26643ee6094bb4e42b5d9800
Artreeus <142108262+Artreeus@users.noreply.github.com>
2025-04-08 02:54:12 +0600

    uppdate

---
+++ src/create.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const Prisma = new PrismaClient();
+
+const main = async () => {
+
+    // create single data in db 
+
+    
+    // const result = await Prisma.post.create({
+    //     data: {
+    //         title:'This is a title',
+    //         content:'This is the content',
+    //         author:'Mahamudul Hasan Ratul' 
+    //     }
+    // })
+
+    
+};
+
+main();
